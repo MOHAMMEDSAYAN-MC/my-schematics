a6c3eaf4f7b55a241928e4d2a93ab6faad8929b7
MOHAMMEDSAYAN-MC <79076008+MOHAMMEDSAYAN-MC@users.noreply.github.com>
2024-03-01 12:48:03 +0530

    Final commit

---
+++ src/my-schematics/index.ts
@@ -0,0 +1,31 @@
+import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { execSync } from 'child_process';
+
+export function mySchematics(_options: any): Rule {
+  return (tree: Tree, _context: SchematicContext) => {
+    const destinationPath = 'D:/Generated';
+    const projectName='newfinalss';
+    try {
+      process.chdir(destinationPath);
+
+     
+      execSync(`ng new ${projectName} --style=css --ssr=false --standalone=false`, { stdio: [0, 1, 2] });
+
+
+      process.chdir(projectName);
+
+
+      execSync(`npm install D:/CodeGeneration/Check/my-schematics/my-schematics/ --save-dev`, { stdio: [0, 1, 2] });
+      execSync(`ng generate my-schematics:form-generator`, { stdio: [0, 1, 2] });
+
+      execSync(`npm install @angular/material @angular/animations bootstrap`,{ stdio: [0, 1, 2] });
+      execSync(`ng add @angular/material --style=scss --theme=custom --globalTypography=true --animations=true --force --defaults`,{ stdio: [0, 1, 2] });
+      execSync(`ng serve -o`, { stdio: [0, 1, 2] });
+      
+    } catch (error) {
+      console.error('Error occurred while creating the new project:', error);
+    }
+
+    return tree;
+  };
+}
